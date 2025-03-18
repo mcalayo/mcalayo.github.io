28fa8e871986a40d442332a4d42d42594199ef88
mcalayo <87282416+mcalayo@users.noreply.github.com>
2025-03-18 11:17:24 -0400

    small change to padding to check deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <section className='py-24'>
+    <section className='p-24'>
       <div className='container'>
         <h1 className='text-3xl font-bold'>NEXT TS STARTER</h1>
       </div>
