132542d6c1eca53c507530464decdd46ea62ff73
mcalayo <87282416+mcalayo@users.noreply.github.com>
2025-03-23 17:35:22 -0400

    update linkedin

--- app/components/LinkedIn.tsx
+++ app/components/LinkedIn.tsx
@@ -1,10 +1,17 @@
 'use client'
 import React from 'react'
+import LinkedInLogo from '../../public/images/linkedin.png'
+import Image from 'next/image'
 
 export default function LinkedIn() {
   return (
     <div>
-      <div className='h-[468px] w-[350px] rounded-xl bg-black'>t</div>
+      <div className='flex h-[468px] w-[350px] flex-col rounded-xl bg-black'>
+        <div className='flex bg-[#395572] p-3'>
+          <Image src={LinkedInLogo} alt='User' width={96} height={96}></Image>
+        </div>
+        <div className='flex bg-red-500'>r</div>
+      </div>
       <>
         <script
           src='https://platform.linkedin.com/badges/js/profile.js'
