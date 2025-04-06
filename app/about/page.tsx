9112343cff7989a45166359de4d187b5649a855b
mcalayo <michaelcalayo@gmail.com>
2025-04-05 20:54:45 -0400

    Contact page styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,19 +6,19 @@ import Resume from '../components/Resume'
 export default async function Page() {
   return (
     <section className='mid:p-12 flex flex-col gap-4 p-6'>
-      <div className='flex h-[453px] flex-col justify-between gap-12 rounded-2xl bg-sky-300/20 p-2 md:flex-row'>
+      <div className='flex h-[470px] linkedin:h-[453px] flex-col justify-between gap-12 rounded-2xl bg-sky-300/20 p-2 md:flex-row'>
         <div className='flex h-full w-full flex-col overflow-hidden p-4'>
           <div className='mid:text-l flex pb-1 text-sm font-bold text-slate-600'>
             <TypingEffect messages={['Professional Experience']}></TypingEffect>
           </div>
-          <p className="scrollbar flex overflow-auto">
+          <p className="scrollbar overflow-auto">
             {`I am a Full Stack Software Engineer specializing in React, Next.js, TypeScript, and AWS, with a passion for building high-performance, scalable web applications. Focused on creating seamless user experiences and optimizing UI through modern design principles and agile methodologies.`}
-            <br/><br/>
-            {`My experience at CoStar Group includes designing and developing server-side rendered applications with a strong emphasis on SEO optimization and reusable component architecture. I worked on integrating automated testing frameworks like Jest and Cypress, improving website indexing for large-scale content, and leading the development of UI features using Tailwind CSS. I also played a key role in launching new digital experiences, enhancing content accessibility, and optimizing API endpoints for better performance.`}
-            <br/><br/>
+            <br /><br />
+            {`My experience at CoStar Group includes designing and developing server-side rendered applications with a strong emphasis on SEO optimization and reusable component architecture. I worked on integrating automated testing frameworks like Jest and Cypress, improving website indexing for large-scale content, and leading the development of UI features using Tailwind CSS. I also played a key role in launching new digital experiences, enhancing content accessibility, and optimizing API endpoints for better performance.`}`
+            <br /><br />
             {`With an AWS Cloud Practitioner certification, I have expertise in API development, cloud technologies, and CI/CD pipelines. I am passionate about building intuitive, efficient, and scalable applications that enhance user engagement and business impact.`}
-            <br/><br/>
-            {`Let's connect and create something amazing together!`}
+            <br /><br />
+            {`Let's `}<a className='text-blue-500 underline' href='/contact'>connect</a> {`and create something amazing together!`}
           </p>
         </div>
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,43 +1,90 @@
 
 import React from "react";
+import TypingEffect from "../components/ReactTyping";
 
 export default async function Page() {
   return (
-    <section className='p-12'>
-      <div className="flex justify-center items-center h-[calc(100vh-160px)] bg-sky-300/20 rounded-2xl p-2">
-        <form className="bg-white p-6 rounded-lg shadow-md w-full">
-          <h2 className="text-2xl font-bold mb-4">Contact Me</h2>
-          <input
-            type="text"
-            name="name"
-            placeholder="Your Name"
-            required
-            className="w-full p-2 border border-gray-300 rounded mb-4"
-          />
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            required
-            className="w-full p-2 border border-gray-300 rounded mb-4"
-          />
-          <textarea
-            name="message"
-            rows={5}
-            placeholder="Your Message"
-            required
-            className="w-full p-2 border border-gray-300 rounded mb-4 resize-none"
-            maxLength={250}
-          ></textarea>
-          <button
-            type="submit"
-            className="w-full bg-sky-700 text-white p-2 rounded hover:bg-sky-800 hover:cursor-pointer"
-          >
-            Send Message
-          </button>
-        </form>
-      </div>
+    <section className='mid:p-12 flex flex-col gap-4 p-6'>
+      <div className='flex h-[428px] flex-col justify-between gap-12 rounded-2xl bg-sky-300/20 p-2 md:flex-row'>
+        <div className='flex h-full w-full flex-col overflow-hidden p-4'>
+          <div className='mid:text-l flex pb-1 text-sm font-bold text-slate-600'>
+            <TypingEffect messages={[`Let's Connect`]}></TypingEffect>
+          </div>
+          <p className="scrollbar overflow-auto">
+          {`Whether you're reaching out about a project, a potential collaboration, looking to book a DJ, or just want to say hey—I'd love to hear from you.`}
+          <br/><br/>
+          {`Drop me a message through the form or email me at michaelcalayo@gmail.com. I'll get back to you as soon as I can!`}
+          <br/><br/>
+          {`Looking forward to connecting.`}
+          <br/><br/>
+          {`-Michael`}
+          </p>
+        </div>
 
+        <form className="bg-white p-6 rounded-lg shadow-md mid:w-[600px] w-full h-[412px] mid:block hidden">
+            <h2 className="text-2xl font-bold mb-4">Send a Message</h2>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+            <textarea
+              name="message"
+              rows={5}
+              placeholder="Your Message"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4 resize-none"
+              maxLength={250}
+            ></textarea>
+            <button
+              type="submit"
+              className="w-full bg-sky-700 text-white p-2 rounded hover:bg-sky-800 hover:cursor-pointer"
+            >
+              Send Message
+            </button>
+          </form>
+      </div>
+      <form className="bg-white p-6 rounded-lg shadow-md mid:w-[600px] w-full h-[412px] mid:hidden block">
+            <h2 className="text-2xl font-bold mb-4">Send a Message</h2>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Name"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+            <textarea
+              name="message"
+              rows={5}
+              placeholder="Your Message"
+              required
+              className="w-full p-2 border border-gray-300 rounded mb-4 resize-none"
+              maxLength={250}
+            ></textarea>
+            <button
+              type="submit"
+              className="w-full bg-sky-700 text-white p-2 rounded hover:bg-sky-800 hover:cursor-pointer"
+            >
+              Send Message
+            </button>
+          </form>
+          
     </section>
   )
 }
